9ab1f0cbd8f849dae1eebe77c9ef253f5eaa01c8
Shiyang Fei <sfei.tek@gmail.com>
2015-07-06 22:21:33 -0400

    Grunt Config Change

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
                 src: [
                     'js/conf.js',
                     'js/services.js',
-                    'js/controllers.js',
+                    'js/controllers/*.js',
                     'js/start-app.js',
                     'js/app.js'
                 ],
@@ -31,7 +31,6 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 files: [
-                    {expand: true, src: ['images/**'], dest: 'dist'},
                     {expand: true, src: ['stylesheets/images/**'], dest: 'dist'},
                     {expand: true, src: ['fonts/**'], dest: 'dist'},
                     {expand: true, src: ['data/**'], dest: 'dist'},
