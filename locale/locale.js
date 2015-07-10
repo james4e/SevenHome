f6ae10dba9d63fadad31bfc8599c48339f1139d5
Shiyang Fei <shiyang@ensofinancial.com>
2015-07-11 04:50:22 +0800

    Clear everything

--- locale/locale.js
+++
@@ -1,5 +0,0 @@
-//Created by Shiyang Fei on 6/16/2015
-var lang = localStorage ? (localStorage.getItem('seven-education-user-lang') || 'en') : 'en';
-var file = 'locale/' + lang + '.js';
-document.write('<script type="text/javascript" src="' + file + '"></script>');
-console.log(file);
